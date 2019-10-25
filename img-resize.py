c7efeda22d1d111b7641a80f42eaa670e1bc6b5b
Nico <35513987+Nicodemos234@users.noreply.github.com>
2019-10-25 20:38:52 -0300

    Image validation

--- img-resize.py
+++ img-resize.py
@@ -1,17 +1,22 @@
 import os, sys
 from PIL import Image
 
+verify = 0
+while(verify == 0):
+  local = input("Qual o diretório da imagem?")
+  if local.find(".png") != -1 or local.find(".jpg") != -1 or local.find(".jpeg") != -1:
+    verify = 1
+  else:
+    print("Informe uma imagem em (.jpg, .jpeg, ou .png)")
 
-local = input("Qual o diretório da imagem?")
+if verify == 1:
+  im1 = Image.open(local)
 
+  height = input("Qual a nova altura da imagem?")
+  width = input("Qual a nova largura da imagem?")
 
-im1 = Image.open(local)
+  im2 = im1.resize((int(width), int(height)), Image.ANTIALIAS)
 
-height = input("Qual a nova altura da imagem?")
-width = input("Qual a nova largura da imagem?")
+  ext = ".png"
 
-im2 = im1.resize((int(width), int(height)), Image.ANTIALIAS)
-
-ext = ".png"
-
-im2.save("out" + ext)
+  im2.save("out" + ext)
